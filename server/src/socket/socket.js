24cae4f0db79ff905b2386b3330d435315b2f3b7
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-08-07 16:44:50 +0700

    Update socket.js

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["https://mern-book-store-fe.onrender.com"],
+        origin: [process.env.CLIENT_URL],
         methods: ["GET", "POST"],
     },
 });
