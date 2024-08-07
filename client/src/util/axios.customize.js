501d452153b2aef3f7c851883815b507f27f157b
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-08-07 14:15:55 +0700

    Update axios.customize.js

--- client/src/util/axios.customize.js
+++ client/src/util/axios.customize.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://mern-book-store-be.onrender.com',
 });
 
 // Add global request interceptor
