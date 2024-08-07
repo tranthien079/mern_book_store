73e6d424b81decdea521b1de4cdec34c6eb28a0e
tranthien079 <106481539+tranthien079@users.noreply.github.com>
2024-08-07 15:50:04 +0700

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { getHomepage } = require('./controllers/homeController');
 const userRouter = require('./routes/userRouter');
 const authorRouter = require('./routes/authorRouter');
-// const bookRouter = require('./routes/bookRouter');
+const bookRouter = require('./routes/bookRouter');
 const messageRouter = require('./routes/messageRoute');
 const path = require('path');
 
